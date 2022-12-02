d5fdd4a945aa202425c2a4bd6a12b5204bdae38c
Camilo Romero <camiloaromero23@gmail.com>
2022-12-01 19:34:53 -0500

    Restore default telscope find function

--- lua/user/alpha.lua
+++ lua/user/alpha.lua
@@ -31,8 +31,8 @@ local dashboard = require "alpha.themes.dashboard"
 dashboard.section.header.val = header
 
 dashboard.section.buttons.val = {
-  -- dashboard.button( "SPC s f", "  Find File", "<CMD>Telescope find_files<CR>" ),
-  dashboard.button( "SPC s f", "  Find File", "<CMD>lua require('user.telescope.custom-finders').find_project_files()<CR>" ),
+  dashboard.button( "SPC s f", "  Find File", "<CMD>Telescope find_files<CR>" ),
+  -- dashboard.button( "SPC s f", "  Find File", "<CMD>lua require('user.telescope.custom-finders').find_project_files()<CR>" ),
   -- dashboard.button( "SPC n", "  New file" , ":ene <BAR> startinsert <CR>"),
   dashboard.button( "SPC P", "  Recent Projects ", "<CMD>Telescope projects<CR>" ),
   dashboard.button( "SPC s r", "  Recently Used Files", "<CMD>Telescope oldfiles<CR>" ),
