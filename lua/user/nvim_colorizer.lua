5855ddd93dcf1a8f0d7e9646346b2bcfc1495af5
Camilo Romero <camiloaromero23@gmail.com>
2022-12-01 19:34:23 -0500

    Remove packer from colorizer filetypes

--- lua/user/nvim_colorizer.lua
+++ lua/user/nvim_colorizer.lua
@@ -6,7 +6,7 @@ M.config = function()
     return
   end
   colorizer.setup {
-    filetypes = { "*" },
+    filetypes = { "*", "!packer" },
     user_default_options = {
       css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
       css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
