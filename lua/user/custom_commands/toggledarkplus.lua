5d149e6d7397139234f87cedc1ea3fe5c8fc1589
Camilo Romero <camiloaromero23@gmail.com>
2022-12-01 19:34:30 -0500

    Update command api

--- lua/user/custom_commands/toggledarkplus.lua
+++ lua/user/custom_commands/toggledarkplus.lua
@@ -2,8 +2,8 @@ vim.api.nvim_create_user_command("ToggleDarkPlus", function()
   local colorscheme = vim.api.nvim_exec("colorscheme", true)
 
   if colorscheme == custom_nvim.colorscheme then
-    vim.cmd [[colorscheme darkplus]]
+    vim.cmd.colorscheme "darkplus"
   else
-    vim.cmd("colorscheme " .. custom_nvim.colorscheme)
+    vim.cmd.colorscheme(custom_nvim.colorscheme)
   end
 end, {})
