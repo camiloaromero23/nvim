59f81f6fb5a95680c8a3ee44192c89f92c2cf0a6
Camilo Romero <camiloaromero23@gmail.com>
2022-12-01 19:34:55 -0500

    Update alpha dashboard

--- lua/user/alpha.lua
+++ lua/user/alpha.lua
@@ -21,21 +21,6 @@ local header = {
     "                      . =* .                      ",
 }
 
-local buttons = {
-  entries = {
-    { "SPC f", "  Find File", "<CMD>Telescope find_files<CR>" },
-    { "SPC n", "  New File", "<CMD>ene!<CR>" },
-    { "SPC P", "  Recent Projects ", "<CMD>Telescope projects<CR>" },
-    { "SPC s r", "  Recently Used Files", "<CMD>Telescope oldfiles<CR>" },
-    { "SPC s t", "  Find Word", "<CMD>Telescope live_grep<CR>" },
-    -- {
-    --   "SPC L c",
-    --   "  Configuration",
-    --   "<CMD>edit " .. require("lvim.config"):get_user_config_path() .. " <CR>",
-    -- },
-  },
-}
-
 local ok, alpha = pcall(require,"alpha")
 if not ok then
   return
@@ -46,9 +31,12 @@ local dashboard = require "alpha.themes.dashboard"
 dashboard.section.header.val = header
 
 dashboard.section.buttons.val = {
-   dashboard.button( "e", "  New file" , ":ene <BAR> startinsert <CR>"),
-   dashboard.button( "q", "  Quit NVIM" , ":qa<CR>"),
-   dashboard.button( "c", "  Configuration", "<CMD>edit " .. "~/.config/nvim/init.lua" .. " <CR>")
+  dashboard.button( "SPC s f", "  Find File", "<CMD>Telescope find_files<CR>" ),
+  -- dashboard.button( "SPC n", "  New file" , ":ene <BAR> startinsert <CR>"),
+  -- dashboard.button( "SPC P", "  Recent Projects ", "<CMD>Telescope projects<CR>" ),
+  dashboard.button( "SPC s r", "  Recently Used Files", "<CMD>Telescope oldfiles<CR>" ),
+  dashboard.button( "SPC s t", "  Find Word", "<CMD>Telescope live_grep<CR>" ),
+  dashboard.button( "SPC L c", "  Configuration", "<CMD>edit " .. "~/.config/nvim/init.lua" .. " <CR>"),
 }
 
 local version = vim.version()
