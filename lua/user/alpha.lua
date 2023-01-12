b006f4ba8726dd297a710d5dc476885d480c20ba
Camilo Romero <camiloaromero23@gmail.com>
2023-01-11 19:21:34 -0500

    Update keymappings

--- lua/user/alpha.lua
+++ lua/user/alpha.lua
@@ -29,13 +29,13 @@ local dashboard = require "alpha.themes.dashboard"
 dashboard.section.header.val = header
 
 dashboard.section.buttons.val = {
-  dashboard.button("SPC s f", "  Find File", "<CMD>Telescope find_files<CR>"),
-  -- dashboard.button( "SPC s f", "  Find File", "<CMD>lua require('user.telescope.custom-finders').find_project_files()<CR>" ),
-  -- dashboard.button( "SPC n", "  New file" , ":ene <BAR> startinsert <CR>"),
-  dashboard.button("SPC P", "  Recent Projects ", "<CMD>Telescope projects<CR>"),
-  dashboard.button("SPC s r", "  Recently Used Files", "<CMD>Telescope oldfiles<CR>"),
-  dashboard.button("SPC s t", "  Find Word", "<CMD>Telescope live_grep<CR>"),
-  dashboard.button("SPC L c", "  Configuration", "<CMD>edit " .. "~/.config/nvim/init.lua" .. " <CR>"),
+  dashboard.button("SPC s f", "  Find File", "<cmd>Telescope find_files<cr>"),
+  -- dashboard.button( "SPC s f", "  Find File", "<cmd>lua require('user.telescope.custom-finders').find_project_files()<cr>" ),
+  -- dashboard.button( "SPC n", "  New file" , ":ene <BAR> startinsert <cr>"),
+  dashboard.button("SPC P", "  Recent Projects ", "<cmd>Telescope projects<cr>"),
+  dashboard.button("SPC s r", "  Recently Used Files", "<cmd>Telescope oldfiles<cr>"),
+  dashboard.button("SPC s t", "  Find Word", "<cmd>Telescope live_grep<cr>"),
+  dashboard.button("SPC L c", "  Configuration", "<cmd>edit " .. "~/.config/nvim/init.lua" .. " <cr>"),
 }
 
 local version = vim.version()
