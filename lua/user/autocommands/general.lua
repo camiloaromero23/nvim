9053c814e45e7af0fb6f491c87905a55e84cd1de
Camilo Romero <camiloaromero23@gmail.com>
2023-01-20 13:09:51 -0500

    Include quick close autocommand for quickfix windows

--- lua/user/autocommands/general.lua
+++ lua/user/autocommands/general.lua
@@ -15,3 +15,25 @@ vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
     vim.cmd "set formatoptions-=cro"
   end,
 })
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = {
+    "qf",
+    "help",
+    "man",
+    "floaterm",
+    "lspinfo",
+    "lir",
+    "lsp-installer",
+    "null-ls-info",
+    "tsplayground",
+    "DressingSelect",
+    "Jaq",
+  },
+  callback = function()
+    vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = true })
+    vim.opt_local.buflisted = false
+  end,
+  group = augroups.quickClose,
+})
+
