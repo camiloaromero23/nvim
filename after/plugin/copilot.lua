4f309c6a54b61c00454c048514bcca90da15e51f
Camilo Romero <camiloaromero23@gmail.com>
2023-02-08 12:45:52 -0500

    Update node command to use fnm

--- after/plugin/copilot.lua
+++ after/plugin/copilot.lua
@@ -3,6 +3,6 @@ if custom_nvim.enable_copilot == false then
 end
 
 vim.g.copilot_no_tab_map = true
--- vim.g.copilot_node_command = "/usr/local/n/versions/node/16.14.2/bin/node"
+-- vim.g.copilot_node_command = "$FNM_DIR/aliases/lts-latest/bin/node"
 
 vim.cmd [[ imap <silent><script><expr> kk copilot#Accept("") ]]
