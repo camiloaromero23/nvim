d2ecbd64cca6002978b14daa6df711c27bccfc13
Camilo Romero <camiloaromero23@gmail.com>
2023-07-14 18:09:47 -0500

    WIP: Include harpoon

---
+++ lua/plugin_declarations/harpoon.lua
@@ -0,0 +1,19 @@
+return {
+  "ThePrimeagen/harpoon",
+  config = function()
+    require("harpoon").setup {
+      global_settings = {
+        -- sets the marks upon calling `toggle` on the ui, instead of require `:w`.
+        save_on_toggle = false,
+        -- saves the harpoon file upon every change. disabling is unrecommended.
+        save_on_change = true,
+        -- filetypes that you want to prevent from adding to the harpoon list menu.
+        excluded_filetypes = { "harpoon" },
+      },
+    }
+  end,
+  -- TODO: Check better keybindings than the ones defined in which-key.lua file
+  -- keys = {
+    -- { "<leader>Tp", "<cmd>TSPlaygroundToggle<cr>", desc = "Toggle Treesitter Playground" },
+  -- },
+}
