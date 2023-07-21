434e013c7651b2e128de453640312f82744d6611
Camilo Romero <camiloaromero23@gmail.com>
2023-07-21 18:57:39 -0500

    Update default theme

--- lua/user/theme/catppuccin/init.lua
+++ lua/user/theme/catppuccin/init.lua
@@ -1,1 +1,1 @@
-vim.cmd.colorscheme "catppuccin-macchiato" -- can be mocha, frappe, macchiato or only catppuccin
+vim.cmd.colorscheme "catppuccin-mocha" -- can be mocha, frappe, macchiato or only catppuccin
