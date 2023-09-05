fe72db2763f71524323f53653aa9e1ca31367d35
Camilo Romero <camiloaromero23@gmail.com>
2023-09-05 16:36:50 -0500

    chore(cmp): Update format

--- lua/user/lsp/cmp.lua
+++ lua/user/lsp/cmp.lua
@@ -66,35 +66,19 @@ cmp.setup {
   },
   formatting = {
     fields = { "kind", "abbr", "menu" },
-    format = lspkind.cmp_format {
-      mode = "symbol", -- show only symbol annotations
-      maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-      preset = "codicons",
-      -- wirth_text = false,
+    format = function(entry, vim_item)
+      local kind = lspkind.cmp_format {
+        mode = "symbol_text",
+        maxwidth = 50,
+        preset = "codicons",
+      }(entry, vim_item)
+      local strings = vim.split(kind.kind, "%s", { trimempty = true })
+      kind.kind = strings[1] or ""
+      kind.menu = "    " .. (strings[2] or "")
 
-      -- The function below will be called before any actual modifications from lspkind
-      -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
-      -- TODO: Add tailwind_colorizer
-      -- before = function(entry, vim_item)
-      -- local ok, tailwind_colorizer = pcall(require, "tailwindcss-colorizer-cmp")
-      -- if not ok then
-      --   return vim_item
-      -- end
-      -- return tailwind_colorizer.formatter(entry, vim_item)
-      -- end,
-    },
-    -- format = function(entry, vim_item)
-    --   -- Kind icons
-    --   vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
-    --   -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
-    --   vim_item.menu = ({
-    --     nvim_lsp = "(LSP)",
-    --     luasnip = "(Snippet)",
-    --     buffer = "(Buffer)",
-    --     path = "(Path)",
-    --   })[entry.source.name]
-    --   return vim_item
-    -- end,
+      -- return require("tailwindcss-colorizer-cmp").formatter(entry, vim_item) -- Enable this when plugin loads custom colors
+      return kind
+    end,
   },
   sources = {
     { name = "nvim_lsp" },
