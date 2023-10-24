643ec23f9e171cf0a2454dd82c6a985b9b2e325f
Camilo Romero <camiloaromero23@gmail.com>
2023-10-24 18:04:37 -0500

    chore: git-blame ignore neo-tree

--- lua/plugin_declarations/git-blame.lua
+++ lua/plugin_declarations/git-blame.lua
@@ -3,7 +3,7 @@ return {
   config = function()
     vim.g.gitblame_message_template = "      <author>, <date> • <summary>"
     vim.g.gitblame_date_format = "%r"
-    vim.g.gitblame_ignored_filetypes = { "NvimTree", "netrw", "packer" }
+    vim.g.gitblame_ignored_filetypes = { "NvimTree", "netrw", "packer", "neo-tree" }
     vim.g.gitblame_set_extmark_options = {
       hl_mode = "combine",
       priority = 10000,
