7c20c1021eaca1424101755cfb6c490aa60ce3a4
Camilo Romero <camiloaromero23@gmail.com>
2023-12-23 14:34:35 -0500

    chore: Update git blame configs

--- lua/plugin_declarations/git-blame.lua
+++ lua/plugin_declarations/git-blame.lua
@@ -1,7 +1,8 @@
 vim.g.blamer_enabled = true
-vim.g.blamer_delay = 0
+vim.g.blamer_delay = 100
 vim.g.blamer_relative_time = 1
 vim.g.blamer_prefix = "     "
+vim.g.blamer_show_in_visual_modes = 0
 
 return {
   "APZelos/blamer.nvim",
