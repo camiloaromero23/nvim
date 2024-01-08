302d44de063df46c1d53eae26cd850053e1ccefb
Camilo Romero <camiloaromero23@gmail.com>
2024-01-08 12:12:44 -0500

    chore: Use gitsigns for git blame

--- lua/plugin_declarations/git-blame.lua
+++
@@ -1,9 +0,0 @@
-vim.g.blamer_enabled = true
-vim.g.blamer_delay = 100
-vim.g.blamer_relative_time = 1
-vim.g.blamer_prefix = "     "
-vim.g.blamer_show_in_visual_modes = 0
-
-return {
-  "APZelos/blamer.nvim",
-}
--- lua/plugin_declarations/gitsigns.lua
+++ lua/plugin_declarations/gitsigns.lua
@@ -54,6 +54,15 @@ return {
       row = 0,
       col = 1,
     },
+    current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+    current_line_blame_opts = {
+      delay = 0,
+    },
+    current_line_blame_formatter_opts = {
+      relative_time = true,
+    },
+    current_line_blame_formatter = "    <author>, <author_time> • <summary>",
+    current_line_blame_formatter_nc = "    Not Commited Yet",
     watch_gitdir = {
       interval = 1000,
       follow_files = true,
