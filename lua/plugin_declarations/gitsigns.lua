32bf309a2880f4ac95bd39574617dec718704a59
Camilo Romero <camiloaromero23@gmail.com>
2024-01-10 11:06:21 -0500

    chore: Update virt_text_priority

--- lua/plugin_declarations/gitsigns.lua
+++ lua/plugin_declarations/gitsigns.lua
@@ -57,6 +57,7 @@ return {
     current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
     current_line_blame_opts = {
       delay = 0,
+      virt_text_priority = 10000,
     },
     current_line_blame_formatter_opts = {
       relative_time = true,
