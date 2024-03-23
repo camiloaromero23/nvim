3d33fbbe5cecfcd3fcb783cc6bd3821c00981976
Sergio David Romero Maldonado <sergiorom92@gmail.com>
2024-03-22 19:05:07 -0500

    feat(silicon): Add Silicon Code Screenshot plugin

--- lua/plugin_declarations/silicon.lua
+++ lua/plugin_declarations/silicon.lua
@@ -2,10 +2,20 @@ return {
   "michaelrommel/nvim-silicon",
   lazy = true,
   cmd = "Silicon",
+  init = function()
+    require("which-key").register {
+      ["<leader>S"] = {
+        ":Silicon<CR>",
+        "Take Code Screenshot 📸",
+        mode = "v",
+      },
+    }
+  end,
   config = function()
     require("silicon").setup {
       font = "CaskaydiaCove Nerd Font=34;Noto Emoji=34",
-      theme = "Dracula",
+      theme = "Catppuccin Mocha",
+      to_clipboard = true,
     }
   end,
 }
