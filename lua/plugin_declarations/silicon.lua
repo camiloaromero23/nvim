6ad6328432fc4f13a48566ec46a753596f0aa185
Camilo Romero <camiloaromero23@gmail.com>
2024-03-22 20:06:14 -0500

    Update silicon config

--- lua/plugin_declarations/silicon.lua
+++ lua/plugin_declarations/silicon.lua
@@ -1,21 +1,18 @@
 return {
   "michaelrommel/nvim-silicon",
-  lazy = true,
   cmd = "Silicon",
-  init = function()
-    require("which-key").register {
-      ["<leader>S"] = {
-        ":Silicon<CR>",
-        "Take Code Screenshot 📸",
-        mode = "v",
-      },
-    }
-  end,
-  config = function()
-    require("silicon").setup {
-      font = "CaskaydiaCove Nerd Font=34;Noto Emoji=34",
-      theme = "Catppuccin Mocha",
-      to_clipboard = true,
-    }
-  end,
+  opts = {
+    font = custom_nvim.silicon_screenshots.font,
+    theme = custom_nvim.silicon_screenshots.theme,
+    to_clipboard = true,
+  },
+  keys = {
+    {
+      "<leader>S",
+      ":Silicon<CR>",
+      silent = true,
+      desc = "Take Code Screenshot 📸",
+      mode = "v",
+    },
+  },
 }
