403278fb9e6709428999489c030ae28e7bf4d2c8
Camilo Romero <camiloaromero23@gmail.com>
2024-04-11 19:24:40 -0500

    feat(treesitter): include vimdoc

--- lua/plugin_declarations/treesitter.lua
+++ lua/plugin_declarations/treesitter.lua
@@ -41,6 +41,7 @@ return {
           "tsx",
           "typescript",
           "vue",
+          "vimdoc",
         }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
         sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
         ignore_install = {}, -- List of parsers to ignore installing
