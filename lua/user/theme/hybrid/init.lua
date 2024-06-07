0c768c89d3602a670cf6dbf47716c47c8ec60f9c
Camilo Romero <camiloaromero23@gmail.com>
2024-06-07 09:26:19 -0500

    feat(hybrid): Update highlights

--- lua/user/theme/hybrid/init.lua
+++ lua/user/theme/hybrid/init.lua
@@ -1,2 +1,4 @@
 vim.cmd.colorscheme "hybrid"
-vim.api.nvim_set_hl(0, "LspInlayHint", { fg = "gray"})
+vim.api.nvim_set_hl(0, "LspInlayHint", { fg = "gray" })
+vim.api.nvim_set_hl(0, "@markup.raw", {})
+vim.api.nvim_set_hl(0, "@markup.raw.block", { fg = "#b5bd68" })
