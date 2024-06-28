63ef2334824534d9eaaf4af193e34fd40f3f022f
Camilo Romero <camiloaromero23@gmail.com>
2024-06-28 14:10:50 -0500

    chore(utils): update deprecated APIs

--- lua/user/utils/functions.lua
+++ lua/user/utils/functions.lua
@@ -2,7 +2,7 @@ local M = {}
 
 function M.smart_quit()
   local bufnr = vim.api.nvim_get_current_buf()
-  local modified = vim.api.nvim_buf_get_option(bufnr, "modified")
+  local modified = vim.api.nvim_get_option_value("modified", { buf = bufnr })
   if modified then
     vim.ui.input({
       prompt = "You have unsaved changes. Quit anyway? (y/n) ",
@@ -21,12 +21,13 @@ function M.isempty(s)
 end
 
 function M.get_buf_option(opt)
-  local status_ok, buf_option = pcall(vim.api.nvim_buf_get_option, 0, opt)
+  local status_ok, buf_option = pcall(vim.api.nvim_get_option_value, opt, { buf = 0 })
+
   if not status_ok then
     return nil
-  else
-    return buf_option
   end
+
+  return buf_option
 end
 
 return M
