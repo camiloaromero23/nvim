3c5a78f7d7a2e3a47fbe5c9767433b023e36c52d
Camilo Romero <camiloaromero23@gmail.com>
2024-06-28 14:09:06 -0500

    chore(gitsigns)!: update api

--- lua/plugin_declarations/gitsigns.lua
+++ lua/plugin_declarations/gitsigns.lua
@@ -4,44 +4,24 @@ return {
   opts = {
     signs = {
       add = {
-        hl = "GitSignsAdd",
         text = icons.ui.BoldLineLeft,
-        numhl = "GitSignsAddNr",
-        linehl = "GitSignsAddLn",
       },
       change = {
-        hl = "GitSignsChange",
         text = icons.ui.BoldLineLeft,
-        numhl = "GitSignsChangeNr",
-        linehl = "GitSignsChangeLn",
       },
       delete = {
-        hl = "GitSignsDelete",
         text = icons.ui.TriangleShortArrowRight,
-        numhl = "GitSignsDeleteNr",
-        linehl = "GitSignsDeleteLn",
       },
       topdelete = {
-        hl = "GitSignsDelete",
         text = icons.ui.TriangleShortArrowRight,
-        numhl = "GitSignsDeleteNr",
-        linehl = "GitSignsDeleteLn",
       },
       changedelete = {
-        hl = "GitSignsChange",
         text = icons.ui.BoldLineLeft,
-        numhl = "GitSignsChangeNr",
-        linehl = "GitSignsChangeLn",
       },
       untracked = {
-        hl = "GitSignsUntracked",
         text = icons.ui.BoldLineLeft,
-        numhl = "GitSignsAddNr",
-        linehl = "GitSignsAddLn",
       },
     },
-    numhl = false,
-    linehl = false,
     signcolumn = true,
     word_diff = false,
     attach_to_untracked = true,
@@ -59,10 +39,7 @@ return {
       delay = 0,
       virt_text_priority = 10000,
     },
-    current_line_blame_formatter_opts = {
-      relative_time = true,
-    },
-    current_line_blame_formatter = "    <author>, <author_time> • <summary>",
+    current_line_blame_formatter = "    <author>, <author_time:%R> • <summary>",
     current_line_blame_formatter_nc = "    Not Commited Yet",
     watch_gitdir = {
       interval = 1000,
@@ -71,7 +48,6 @@ return {
     sign_priority = 6,
     update_debounce = 200,
     status_formatter = nil, -- Use default
-    yadm = { enable = false },
   },
   event = "User FileOpened",
 }
