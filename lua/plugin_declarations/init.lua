96d3776221900e51c21ff55ac7f88863a218c6a3
Camilo Romero <camiloaromero23@gmail.com>
2024-06-28 14:09:28 -0500

    feat(tmux-navigator): include tmux-navigator

--- lua/plugin_declarations/init.lua
+++ lua/plugin_declarations/init.lua
@@ -97,4 +97,19 @@ return {
     end,
     event = "User FileOpened",
   },
+  {
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+    },
+    keys = {
+      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+    },
+  },
 }
