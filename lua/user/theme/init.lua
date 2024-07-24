9b46915255af34044d4281f71e7d501bca35ba48
Camilo Romero <camiloaromero23@gmail.com>
2024-07-23 21:33:56 -0500

    chore(alpha): update header highlight name

--- lua/user/theme/init.lua
+++ lua/user/theme/init.lua
@@ -3,7 +3,7 @@ local ok, _ = pcall(require, theme)
 
 if not ok then
   vim.cmd.colorscheme(custom_nvim.colorscheme)
-  vim.api.nvim_set_hl(0, "DashboardLogo", { fg = "#158DA2" })
+  vim.api.nvim_set_hl(0, "AlphaHeader", { fg = "#158DA2" })
   vim.api.nvim_set_hl(0, "GitSignsChange", { fg = "#dbb671" })
   vim.api.nvim_set_hl(0, "FloatTitle", { fg = "#bb70d2" })
   vim.api.nvim_set_hl(0, "PackageInfoOutdatedVersion", { fg = "#D19A66" })
