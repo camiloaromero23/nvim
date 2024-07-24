7139a3d30ad6f6cd2076fd2f51097bc3b98632e3
Camilo Romero <camiloaromero23@gmail.com>
2024-07-23 19:40:48 -0500

    fix(markdown-preview): update install command

--- lua/plugin_declarations/init.lua
+++ lua/plugin_declarations/init.lua
@@ -81,7 +81,7 @@ return {
   },
   {
     "iamcco/markdown-preview.nvim",
-    build = "cd app && npm install",
+    build = "cd app && npx --yes yarn install",
     ft = "markdown",
     config = function()
       vim.g.mkdp_auto_start = 1
