675ccdbfcbfa09b4f7ad1e497079e7b7ae20947e
Camilo Romero <camiloaromero23@gmail.com>
2024-11-05 14:42:13 -0500

    chore(lualine): use only active buffer LSPs

--- lua/user/lualine.lua
+++ lua/user/lualine.lua
@@ -89,7 +89,7 @@ local components = {
   lsp = {
     function(msg)
       msg = msg or "LS Inactive"
-      local buf_clients = vim.lsp.get_clients()
+      local buf_clients = vim.lsp.get_clients { bufnr = 0 }
       if next(buf_clients) == nil then
         -- TODO: clean up this if statement
         if type(msg) == "boolean" or #msg == 0 then
