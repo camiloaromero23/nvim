9a57b45518bc3c1f9a451476de4e533c0c9f80d4
Camilo Romero <camiloaromero23@gmail.com>
2025-02-10 19:49:11 -0500

    chore(snacks): extract dashboard config

--- lua/plugin_declarations/snacks.lua
+++ lua/plugin_declarations/snacks.lua
@@ -1,4 +1,4 @@
-local icons = require("user.icons").ui
+local dashboard_config = require("user.snacks.dashboard")
 return {
   {
     "folke/snacks.nvim",
@@ -8,66 +8,7 @@ return {
     ---@module 'snacks.nvim'
     opts = {
       bigfile = { enabled = true },
-      ---@class snacks.dashboard.Config
-      dashboard = {
-        row = 3,
-        enabled = true,
-        preset = {
-          header = [[
-                 #######                
-          ###    #######    ###         
-       (######## ####### ########%      
-        #########################       
-          #####################         
-    #################################   
-    #################################   
-    #################################   
-          #####################         
-        #########################       
-       ######### ####### ########.      
-          ###    #######    ###         
-                 #######                
-      ]],
-          keys = {
-            {
-              icon = icons.FindFile,
-              desc = "Find File",
-              key = "<leader>sf",
-              action = ":lua Snacks.dashboard.pick('files')",
-            },
-            {
-              icon = "",
-              desc = "Recently Used Files",
-              key = "<leader>sr",
-              action = ":lua Snacks.dashboard.pick('oldfiles')",
-            },
-            {
-              icon = icons.FindText,
-              desc = "Find Word",
-              key = "<leader>st",
-              action = ":lua Snacks.dashboard.pick('live_grep')",
-            },
-            {
-              icon = "",
-              desc = "Configuration",
-              key = "<leader>C",
-              action = "<cmd>edit ~/.config/nvim/init.lua <cr>",
-            },
-          },
-        },
-        sections = {
-          { section = "header" },
-          { section = "keys", gap = 1, padding = 1, width = 10 },
-          {
-            section = "terminal",
-            cmd = "nvim --version | head -n 1 | sed 's/-dev//'",
-            width= 12,
-            height = 5,
-            indent = 24,
-            -- hl = " s",
-          },
-        },
-      },
+      dashboard = dashboard_config,
       indent = { enabled = false },
       input = { enabled = false },
       notifier = {
