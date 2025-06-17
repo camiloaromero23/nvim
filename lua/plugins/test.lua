9803a4947caf86874f1352992048e6913dbd405a
Camilo Romero <camiloaromero23@gmail.com>
2025-06-17 16:05:01 -0500

    chore: add vitest support

---
+++ lua/plugins/test.lua
@@ -0,0 +1,13 @@
+return {
+  "nvim-neotest/neotest",
+  dependencies = {
+    "marilari88/neotest-vitest",
+  },
+  opts = function()
+    return {
+      adapters = {
+        require "neotest-vitest",
+      },
+    }
+  end,
+}
